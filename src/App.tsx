dd5b1458a0a84dd04be20e097bf9e914a59b0bb2
Sopheaktra Sarom <sopheaktrasarom@gmail.com>
2023-01-02 22:44:25 +0700

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
+import "./App.css";
 export default function App() {
   return (
-    <div className="h-screen snap-y snap-mandatory overflow-y-scroll">
-      <div className="h-screen flex items-center justify-center snap-start bg-red-500 text-6xl ">
+    <>
+      <section className="d flex items-center justify-center  bg-red-500 text-6xl ">
         1
-      </div>
-      <div className="h-screen flex items-center justify-center snap-start bg-blue-500 text-6xl">
+      </section>
+      <section className="d flex items-center justify-center  bg-blue-500 text-6xl">
         2
-      </div>
-      <div className="h-screen flex items-center justify-center snap-start bg-red bg-yellow-300 text-6xl">
+      </section>
+      <section className="d flex items-center justify-center  bg-red bg-yellow-300 text-6xl ">
         3
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
